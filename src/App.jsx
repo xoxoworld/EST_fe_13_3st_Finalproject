73549ba1fcb60abc4d3f157068db703b583abe13
Jeongwon <picasomati@gmail.com>
2026-08-02 15:09:33 +0900

    feat: 폴더 생성 및 공통 스타일 추가 (#1)
    feat: npm 폴더 생성 및 공통 스타일 추가(배색, 폰트, 그리드)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="font-display dtext-5xl">깃깔나는 레시피</h1>
+        <p className="text-lg">3차 프로젝트</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
